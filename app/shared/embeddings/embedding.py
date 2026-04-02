65d9d61c891403a274e2ca53884090d587e28f50
Red-blaze56 <87176678+Red-blaze56@users.noreply.github.com>
2026-04-02 17:10:42 +0530

    changes

--- app/shared/embeddings/embedding.py
+++ app/shared/embeddings/embedding.py
@@ -1,6 +1,8 @@
 from sentence_transformers import SentenceTransformer
+import asyncio
 
-def embed_text(text: str)-> list[float]:
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    embedding = model.encode(text)
-    return embedding.tolist()
+model = SentenceTransformer('all-MiniLM-L6-v2')
+def _embed(text: str):
+    return model.encode(text, normalize_embeddings=True).tolist()
+async def embed_text(text: str) -> list[float]:
+    return await asyncio.to_thread(_embed, text)
